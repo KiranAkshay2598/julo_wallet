94308755ad250e880a04901ef00f777d98fef1f3
kirankrazeey05 <kirankrazeey@gmail.com>
2022-01-26 15:14:58 +0530

    Added changes to unit test

--- walletapp/tests.py
+++ walletapp/tests.py
@@ -7,37 +7,21 @@ from walletapp.models import Wallet, Account
 
 class WalletTestCase(APITestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username='test_user', password="some_strong_psw")
+        self.user = User.objects.create_user(username='testcustomer1')
         self.token = Token.objects.create(user=self.user)
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
-        self.account = Account.objects.create(full_name="testkiran", user=self.user)
-        self.wallet = Wallet.objects.create(account=self.account, balance=0)
+        self.account = Account.objects.create(customer_xid="testcustomer1", user=self.user)
+        self.wallet = Wallet.objects.create(account=self.account)
         self.wallet.status = True
         self.wallet.save()
 
     def test_registration_success(self):
-        data = {"username": "testcaseregister",
-                "password": "some_strong_psw",
-                "fullname": "Test Name"}
+        data = {"customer_xid": "testcustomer2"}
         response = self.client.post("/api/v1/init", data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_registration_failure(self):
-        data = {"username": "test_user",
-                "password": "some_strong_psw",
-                "fullname": "Test Name"}
-        response = self.client.post("/api/v1/init", data)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_registration_failure1(self):
-        data = {"password": "some_strong_psw", "fullname": "Test Name"}
-        response = self.client.post("/api/v1/init", data)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        data = {"username": "testcaseregister", "fullname": "Test Name"}
-        response = self.client.post("/api/v1/init", data)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        data = {"username": "testcaseregister", "password": "some_strong_psw"}
-        response = self.client.post("/api/v1/init", data)
+        response = self.client.post("/api/v1/init", data={})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_wallet_view_post(self):
@@ -61,7 +45,8 @@ class WalletTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_wallet_view_patch(self):
-        response = self.client.patch("/api/v1/wallet")
+        data = {'is_disabled': True}
+        response = self.client.patch("/api/v1/wallet", data=data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_wallet_view_patch_failure(self):
@@ -71,32 +56,32 @@ class WalletTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_deposit(self):
-        data = {"amount": 1000}
+        data = {"amount": 1000, 'reference_id':'test_ref_id_1'}
         response = self.client.post("/api/v1/wallet/deposits", data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_deposit_failure(self):
         response = self.client.post("/api/v1/wallet/deposits")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_deposit_failure1(self):
-        data = {"amount": 1000}
+        data = {"amount": 1000, 'reference_id':'test_ref_id_1'}
         self.wallet.status = False
         self.wallet.save()
         response = self.client.post("/api/v1/wallet/deposits", data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_withdrawl(self):
-        data = {"amount": 300}
+        data = {"amount": 300, 'reference_id':'test_ref_id_1'}
         response = self.client.post("/api/v1/wallet/withdrawals", data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_withdrawl_failure(self):
         response = self.client.post("/api/v1/wallet/withdrawals")
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_withdrawl_failure1(self):
-        data = {"amount": 1000}
+        data = {"amount": 300, 'reference_id':'test_ref_id_1'}
         self.wallet.status = False
         self.wallet.save()
         response = self.client.post("/api/v1/wallet/withdrawals", data)
